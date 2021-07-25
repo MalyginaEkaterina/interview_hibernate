0aa7d994b7f4917edcb4c1bdc4cfc314d7e8f5e9
Ekaterina Malygina <katty2007v@yandex.ru>
2021-07-25 23:26:14 +0300

    initial commit

---
+++ interview_hibernate/src/main/java/ru/geekbrains/interview_hibernate/InterviewHibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.interview_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewHibernateApplication.class, args);
+	}
+
+}
